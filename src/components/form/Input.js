5580d2c75277bfbc98fa75963b262a2bd0ed376c
ArthThapa <arththapa7@gmail.com>
2022-03-09 20:43:10 +0530

    Clean stuff and setup tailwind

---
+++ src/components/form/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <select name="type" id="type">
+      <option value="arth">Arth</option>
+      <option value="anmol">Anmol</option>
+      <option value="ash">Ash</option>
+    </select>
+  );
+};
+
+export default Input;
