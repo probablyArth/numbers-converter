dde0339feeb56c3e5604daf74912a88ebb79f6c2
ArthThapa <arththapa7@gmail.com>
2022-03-10 19:43:07 +0530

    Unused useEffect

--- src/components/Toggle/index.js
+++ src/components/Toggle/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { themeContext } from "../../contexts/ThemeContext";
 
 const Toggle = () => {
--- src/components/form/utils.js
+++ src/components/form/utils.js
@@ -1,7 +1,3 @@
-// const decimalToOctal = (decimal) => {
-//   return decimal.toString(8);
-// };
-
 export const convertBro = (number, from, to) => {
   const num = parseInt(number, from);
   return num.toString(to);
